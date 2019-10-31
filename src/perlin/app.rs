780d5bab63235a723f8d8f8bd51c0bb52c2d2d36
nkjmsss <nakajima-sota018@g.ecc.u-tokyo.ac.jp>
2019-10-31 13:48:56 +0900

    :sparkles: Add perlin visualization

---
+++ src/perlin/app.rs
@@ -0,0 +1,95 @@
+use nannou::prelude::*;
+
+mod perlin;
+use crate::perlin::PerlinRect;
+
+fn main() {
+    nannou::app(model).update(update).run();
+}
+
+struct Model {
+    window: window::Id,
+    message: Message,
+    window_event: Option<WindowEvent>,
+    perlin_rect: PerlinRect,
+}
+
+#[derive(Debug, Copy, Clone)]
+enum Message {
+    Initialize,
+    Clear,
+    RenderReady,
+    Nothing,
+}
+
+fn model(app: &App) -> Model {
+    let window = app
+        .new_window()
+        .with_dimensions(720, 720)
+        .view(view)
+        .event(window_event)
+        .build()
+        .unwrap();
+    let message = Message::Initialize;
+    let window_event = None;
+    let perlin_rect = PerlinRect::new();
+
+    Model {
+        window,
+        message,
+        window_event,
+        perlin_rect,
+    }
+}
+
+fn update(_app: &App, model: &mut Model, _update: Update) {
+    if let Some(event) = model.window_event.clone() {
+        model.window_event = None;
+        match event {
+            WindowEvent::KeyPressed(_key) => {
+                model.perlin_rect.set_seed_rand();
+                model.message = Message::Clear;
+                return;
+            }
+            _ => {}
+        }
+    }
+
+    match model.message {
+        Message::Initialize => {
+            model.message = Message::Clear;
+        }
+        Message::Clear => {
+            model.message = Message::RenderReady;
+        }
+        Message::RenderReady => {
+            model.message = Message::Nothing;
+        }
+        _ => {
+            model.message = Message::Nothing;
+        }
+    };
+}
+
+fn window_event(_app: &App, model: &mut Model, event: WindowEvent) {
+    model.window_event = Some(event);
+}
+
+fn view(app: &App, model: &Model, frame: &Frame) {
+    let draw = app.draw();
+
+    match model.message {
+        Message::Clear => {
+            draw.background().color(WHITE);
+        }
+        Message::RenderReady => {
+            model
+                .perlin_rect
+                .draw(&draw, app.window(model.window).unwrap().inner_size_pixels());
+        }
+        _ => {}
+    };
+
+    // Write to the window frame.
+    draw.to_frame(app, &frame).unwrap();
+}
---
+++ src/perlin/perlin.rs
@@ -0,0 +1,51 @@
+#![allow(dead_code)]
+use nannou::noise::{NoiseFn, Perlin, Seedable};
+use nannou::prelude::*;
+use rand::{thread_rng, Rng};
+
+#[derive(Debug, Copy, Clone)]
+pub struct PerlinRect {
+    perlin_gen: Perlin,
+}
+
+impl PerlinRect {
+    pub fn new() -> Self {
+        let perlin_gen = Perlin::new();
+        PerlinRect { perlin_gen }
+    }
+
+    pub fn draw(&self, draw: &app::Draw, window_size: (u32, u32)) {
+        let mesh_count = 500.0;
+        let perlin_range = 12.0;
+        let (window_width, window_height) = window_size;
+        let w = (window_width as f32) / mesh_count;
+        let h = (window_height as f32) / mesh_count;
+
+        for x in (0..).take(mesh_count as usize).map(|i| (i + 1) as f32) {
+            for y in (0..).take(mesh_count as usize).map(|i| (i + 1) as f32) {
+                let color = (self.perlin_gen.get([
+                    (x / mesh_count * perlin_range) as f64,
+                    (y / mesh_count * perlin_range) as f64,
+                ]) + 1.0)
+                    / 2.0;
+                draw.rect()
+                    .w(w)
+                    .h(h)
+                    .color(rgb(color, color, color))
+                    // .color(hsl(color as f32, 0.7, 0.7))
+                    .x((x - mesh_count / 2.0) * w)
+                    .y((y - mesh_count / 2.0) * h);
+            }
+        }
+    }
+
+    pub fn set_seed(&mut self, seed: u32) -> Self {
+        self.perlin_gen = Perlin::new().set_seed(seed);
+        *self
+    }
+
+    pub fn set_seed_rand(&mut self) -> Self {
+        let mut rng = thread_rng();
+        self.set_seed(rng.gen())
+    }
+}
